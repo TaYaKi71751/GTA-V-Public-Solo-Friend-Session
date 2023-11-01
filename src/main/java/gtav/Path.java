870ed2a6cf21fcacbf696fe31ca75fa6506affa1
TaYaKi71751 <shantian.tw@gmail.com>
2023-11-02 03:03:06 +0900

    Fix split with file separator with Pattern.quote

--- src/main/java/gtav/Path.java
+++ src/main/java/gtav/Path.java
@@ -2,10 +2,13 @@ package gtav;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class Path {
 	public static File getUpperPath(File path){
-		String[] psa = (path + "").split(File.separator);
+		System.out.println(path.toString());
+		// https://stackoverflow.com/questions/10336293/splitting-filenames-using-system-file-separator-symbol
+		String[] psa = path.toString().split(Pattern.quote(System.getProperty("file.separator")));
 		if(psa.length > 0) psa[psa.length - 1] = null;
 		// https://stackoverflow.com/questions/24112715/java-8-filter-array-using-lambda
 		psa = Arrays.stream(psa).filter(p -> p != null).toArray(String[]::new);
